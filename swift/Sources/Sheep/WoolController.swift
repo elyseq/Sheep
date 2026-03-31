0d066b94cc8a4c9e2380a05c384adf0f6c5dd14c
mscott716 <mscott4@macalester.edu>
2026-03-31 13:33:55 -0500

    updated sheep!

--- swift/Sources/Sheep/WoolController.swift
+++ swift/Sources/Sheep/WoolController.swift
@@ -17,24 +17,37 @@ import Foundation
 @Godot
 class WoolController: CharacterBody2D {
     override func _ready() {
-        let sheepsprite = Sprite2D()
-        sheepsprite.texture = GD.load(path: "res://assets/TransparentSheep.png") as? Texture2D
-        sheepsprite.position = Vector2(x: 500, y: 500)
-        sheepsprite.scale = Vector2(x: 1.5, y: 1.5)
-        addChild(node:sheepsprite)
+        
+        let sheepbody = Sprite2D()
+        sheepbody.texture = GD.load(path: "res://assets/sheepBody.png") as? Texture2D
+        sheepbody.position = Vector2(x: 1000, y: 700)
+        sheepbody.scale = Vector2(x: 2, y: 2)
+        addChild(node:sheepbody)
+        
+//        let sheepsprite = Sprite2D()
+//        sheepsprite.texture = GD.load(path: "res://assets/TransparentSheep.png") as? Texture2D
+//        sheepsprite.position = Vector2(x: 500, y: 500)
+//        sheepsprite.scale = Vector2(x: 1.5, y: 1.5)
+//        addChild(node:sheepsprite)
         
         let woolLocations = readFile(fileName: "sheepmatrix.txt")
         for y in 0...woolLocations.count-1{
             let ypos = 10 * y - 220
             for x in 0...woolLocations[y].count-1{
-                let xpos = 10 * x - 90
+                let xpos = 10 * x - 160
                 if(woolLocations[y][x] == "1"){
                     let wool = makeWoolNode(Vector2(x: Float(xpos), y: Float(ypos)))
-                    sheepsprite.addChild(node: wool)
+                    sheepbody.addChild(node: wool)
                 }
             }
         
         }
+        
+        let sheephead = Sprite2D()
+        sheephead.texture = GD.load(path: "res://assets/sheepHead.png") as? Texture2D
+        sheephead.position = Vector2(x: 1000, y: 700)
+        sheephead.scale = Vector2(x: 2, y: 2)
+        addChild(node:sheephead)
     }
     
     func readFile(fileName: String) -> [[String]] {
