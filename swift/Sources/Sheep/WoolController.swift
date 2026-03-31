4bfd04f573a6b2da4549f94e02c409f150c2925e
mscott716 <mscott4@macalester.edu>
2026-03-31 16:25:10 -0500

    fixed position of wool on sheep

--- swift/Sources/Sheep/WoolController.swift
+++ swift/Sources/Sheep/WoolController.swift
@@ -24,18 +24,12 @@ class WoolController: CharacterBody2D {
         sheepbody.scale = Vector2(x: 2, y: 2)
         addChild(node:sheepbody)
         
-//        let sheepsprite = Sprite2D()
-//        sheepsprite.texture = GD.load(path: "res://assets/TransparentSheep.png") as? Texture2D
-//        sheepsprite.position = Vector2(x: 500, y: 500)
-//        sheepsprite.scale = Vector2(x: 1.5, y: 1.5)
-//        addChild(node:sheepsprite)
-        
         // Wool
         let woolLocations = readFile(fileName: "sheepmatrix.txt")
         for y in 0...woolLocations.count-1{
             let ypos = 10 * y - 220
             for x in 0...woolLocations[y].count-1{
-                let xpos = 10 * x - 160
+                let xpos = 10 * x - 180
                 if(woolLocations[y][x] == "1"){
                     let wool = makeWoolNode(Vector2(x: Float(xpos), y: Float(ypos)))
                     sheepbody.addChild(node: wool)
