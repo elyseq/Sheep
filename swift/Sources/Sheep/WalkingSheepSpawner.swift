59fcdb9a8649c718836477fd5b528145686326b4
elyseq <equigley@macalester.edu>
2026-04-01 20:09:07 -0500

    made walking work in array

--- swift/Sources/Sheep/WalkingSheepSpawner.swift
+++ swift/Sources/Sheep/WalkingSheepSpawner.swift
@@ -10,17 +10,52 @@ import SwiftGodot
 @Godot
 public class WalkingSheepSpawner: CharacterBody2D {
     
-    //public var sheepList: [WalkingSheep] = []
+    var sheepList: [WalkingSheep] = []
 
     public override func _ready() {
-        //for i in 0..<4 {
-        let sheep = WalkingSheep()
-        //sheep.position = Vector2(x: 800 + Float(i) * 150, y: 700)
-        //sheep.position = Vector2(x: 800, y: 700)
-        sheep.visible = true
-        //sheep.scale = Vector2(x: 2, y: 2)
-        self.addChild(node: sheep)
-        //sheepList.append(sheep)
-        //}
+//        //1 SHEEP (WORKING)
+//        //for i in 0..<4 {
+//            let sheep = WalkingSheep()
+//            //sheep.position = Vector2(x: 800 + Float(i) * 150, y: 700) //doesn't work
+//            //sheep.position = Vector2(x: 800, y: 700) //doesn't work
+//            //sheep.position = Vector2(x: 1300, y: 700) //doesn't work
+//            //sheep.position = Vector2(x: Float(i) * 150, y: Float(i) * 50)
+//            sheep.position = Vector2(x: 0, y: 0) //WORKS
+//            sheep.visible = true
+//            sheep.scale = Vector2(x: 0.7, y: 0.7)
+//            self.addChild(node: sheep)
+//            //sheepList.append(sheep)
+//            GD.print("sheep added")
+//        //}
+        
+        //ARRAY OF SHEEP (WORKING, looks off)
+        for i in 0..<4 {
+            let sheep = WalkingSheep()
+            //sheep.position = Vector2(x: 800 + Float(i) * 150, y: 700) //doesn't work
+            //sheep.position = Vector2(x: 800, y: 700) //doesn't work in array
+            //sheep.position = Vector2(x: 1300, y: 700) //doesn't work in array
+            sheep.position = Vector2(x: Float(i) * 150, y: Float(i) * 50)
+            sheep.visible = true
+            sheep.scale = Vector2(x: 0.7, y: 0.7)
+            self.addChild(node: sheep)
+            sheepList.append(sheep)
+            GD.print("sheep added")
+        }
+        
+        //ARRAY OF SHEEP RANDOM POINT (NOT WORKING)
+//        for i in 0..<4 {
+//            let sheep = WalkingSheep()
+//
+//            sheep.position = Vector2(x: 800, y: 500 //these points don't work (was using to test range)
+//                //x: Float.random(in: 300...800), //wrong range
+//                //y: Float.random(in: 0...1) //wrong range
+//            )
+//            
+//            sheep.visible = true
+//            sheep.scale = Vector2(x: 0.7, y: 0.7)
+//            addChild(node: sheep)
+//            sheepList.append(sheep)
+//            GD.print("sheep added")
+//        }
     }
 }
